643dda4c60351a6316c8c0cebd08067606938f4a
ZhenXun Wong <zhenxun_9119@hotmail.com>
2018-07-03 13:41:40 +0800

    add index datatable and datatable locale

---
+++ resources/lang/en/datatable.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'members' => [
+        'no' => 'No.',
+        'name' => 'Name',
+        'email' => 'Email',
+        'joining_date' => 'Joining Date',
+        'edit' => 'Edit'
+    ],
+
+];
--- resources/views/backend/members/index.blade.php
+++ resources/views/backend/members/index.blade.php
@@ -13,6 +13,46 @@
                   @slot('url_back') {{ route('admin.dashboard') }} @endslot
                 @endcomponent
 
+                <table id="datatable" class="table table-striped table-bordered responsive display" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>{{ trans('datatable.members.no') }}</th>
+                                <th>{{ trans('datatable.members.name') }}</th>
+                                <th>{{ trans('datatable.members.email') }}</th>
+                                <th>{{ trans('datatable.members.joining_date') }}</th>
+                                <th>{{ trans('datatable.members.edit') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($members as $member)
+                                <tr>
+                                    <td class="datatable-iteration">{{ $loop->iteration }}</td>
+                                    <td>{{ $member->ename }}</td>
+                                    <td>{{ $member->email }}</td>
+                                    <td>{{ $member->joining_date }}</td>
+                                    <td class="datatable-editbar">
+                                        <div class="d-flex justify-content-center flex-nowrap">
+                                            <div class="mr-3">
+                                                <a href="" title="編輯">
+                                                    <i class="fa fa-pencil"></i>
+                                                </a>
+                                            </div>
+                                            <div class="mr-3">
+                                                <a href="" title="檢視">
+                                                    <i class="fa fa-eye"></i>
+                                                </a>
+                                            </div>
+                                            <div class="">
+                                                <a href="" title="刪除">
+                                                    <i class="fa fa-trash"></i>
+                                                </a>
+                                            </div>
+                                        </div> 
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                </table>
                 
 
             </div>
