a88d9205238aa941ef6365d38f77821cf6652cec
ZhenXun Wong <zhenxun_9119@hotmail.com>
2018-07-03 10:51:33 +0800

    add members view

---
+++ resources/views/backend/members/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.backend.app')
+
+@section('content')
+
+    <div class="container my-5 ">
+        <div class="row">
+            <div class="col-12 p-4 form-div">
+
+                @include('layouts.backend.components.alert.alert')
+
+                @component('layouts.backend.components.index_header')
+                  @slot('url_create') {{ route('admin.news.create') }} @endslot
+                  @slot('url_back') {{ route('admin.dashboard') }} @endslot
+                @endcomponent
+
+                
+
+            </div>
+        </div>
+    </div>
+
+@endsection
