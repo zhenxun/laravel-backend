1a328d2af052482bcbcb4ea4cfd0f78ced77eb52
ZhenXun Wong <zhenxun_9119@hotmail.com>
2018-07-03 11:32:29 +0800

    add members csv import route

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::group(['prefix' => 'backend', 'namespace' => 'Backend', 'as' => 'admin.']
         Route::resource('news', 'NewsController');
         Route::resource('users', 'UsersController');
         Route::resource('members', 'MembersController');
+        Route::post('/members/csv/import', 'MembersController@importCsv')->name('members.csv.import');
         Route::resource('attachments', 'AttachmentsController', ['only' => ['store', 'destroy']]);
         Route::get('api/attachments/{pages}', 'AttachmentsController@getArticles')->name('attachment.api');
         Route::resource('settings', 'SettingsController', ['only' => ['index', 'edit', 'update']]);
