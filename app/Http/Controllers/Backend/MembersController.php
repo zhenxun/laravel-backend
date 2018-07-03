e33c9d514d366e9b85605277ab1af02d74bc3aaf
ZhenXun Wong <zhenxun_9119@hotmail.com>
2018-07-03 10:58:55 +0800

    add members controller aditional features

--- app/Http/Controllers/Backend/MembersController.php
+++ app/Http/Controllers/Backend/MembersController.php
@@ -3,9 +3,68 @@
 namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BackendController;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Redirect;
 
-class MembersController extends Controller
+
+
+class MembersController extends BackendController
 {
-    //
+    public function __construct(){
+
+    }
+
+    public function index(){
+        return view('backend.members.index');
+    }
+
+    public function create(){
+        $route = URL::route('admin.members.create');
+        return view('backend.members.create', compact('route'));
+    }
+
+    public function edit($id){
+        $route = URL::route('admin.members.update', $id);
+        return view('backend.members.edit', compact('route'));
+    }  
+
+    public function store(){
+
+    }
+
+    public function importCsv(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+
+    }
+
+    private function csvToArray($filename = '', $delimiter = ','){
+        
+        if (!file_exists($filename) || !is_readable($filename))
+        return false;
+
+        $header = null;
+        $data = array();
+        if (($handle = fopen($filename, 'r')) !== false)
+        {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false)
+            {
+                if (!$header)
+                    $header = $row;
+                else
+                    $data[] = array_combine($header, $row);
+            }
+            fclose($handle);
+        }
+
+        return $data;
+    }
+
 }
