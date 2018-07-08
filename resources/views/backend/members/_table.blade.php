e384c28188ab7a9ce325ce13b4a196d85fd3caf9
Zhen-Xun WONG <wzx@playsure.tw>
2018-07-08 09:49:18 +0800

    add members show pages mobile table

--- resources/views/backend/members/_table.blade.php
+++ resources/views/backend/members/_table.blade.php
@@ -1,4 +1,4 @@
-<div class="table-responsive">
+<div class="table-responsive members-show-table-desktop">
 <table class="table table-sm table-borderless table-striped">
     <tbody>
       <tr>
@@ -37,7 +37,7 @@
       <tr>
           <th>{{ trans('form.members.joining_date.label') }}</th>
           <td>{{ $member->joining_date }}</td>
-          <th width="150">{{ trans('form.members.sms_email.label') }}</th>
+          <th width="120">{{ trans('form.members.sms_email.label') }}</th>
           <td colspan="2">
               @if($member->sms_email)
                 <i class="fa fa-check"></i>
@@ -62,4 +62,121 @@
       </tr>
     </tbody>
   </table>
+</div>
+
+<div class="members-show-table-mobile">
+  <div class="d-flex flex-column">
+      <div class="p-2 text-center">
+        {!! QrCode::size(120)->generate($member->member_code); !!}
+      </div>
+      <div class="p-1">
+        <p>
+        <span class="mr-2">
+          <strong>{{ trans('form.members.ename.label') }}:</strong>
+        </span>
+        {{ $member->ename }}
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.cname.label') }}:</strong>
+          </span>
+          {{ $member->cname  }}
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.email.label') }}:</strong>
+          </span>
+          <a href="mailto:{{ $member->email }}">{{ $member->email }}</a>
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.contact_number.label') }}:</strong>
+          </span> {{ $member->contact_no }}
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+                  <strong>{{ trans('form.members.gender.label.default') }}:</strong>
+          </span>
+          @switch($member->gender)
+              @case('male')
+              {{ trans('form.members.gender.label.male') }}
+              @break
+
+              @case('female')
+              {{ trans('form.members.gender.label.female')}}
+              @break
+
+              @case('other')
+              {{ trans('form.members.gender.label.default') }}
+              @break
+          @endswitch
+        </p>
+      </div>
+
+      <div class="p-1">
+          <p>
+            <span class="mr-2">
+            <strong>{{ trans('form.members.age_group.label.default') }}:</strong>
+            </span>
+            {{ $member->age_group }}
+          </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.joining_date.label') }}:</strong>
+          </span>
+          {{ $member->joining_date }}
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.sms_email.label') }}:</strong>
+          </span>
+          @if($member->sms_email)
+              <i class="fa fa-check"></i>
+          @else
+              <i class="fa fa-times"></i>
+          @endif
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.consent.label') }}:</strong>
+          </span>
+          @if($member->consent)
+              <i class="fa fa-check"></i>
+          @else
+              <i class="fa fa-times"></i>
+          @endif
+        </p>
+      </div>
+
+      <div class="p-1">
+        <p>
+          <span class="mr-2">
+            <strong>{{ trans('form.members.remarks.label') }}:</strong>
+          </span>
+          {{ $member->remarks }}
+        </p>
+      </div>
+
+  </div>
 </div>
